53f555c1a7b75e97d26d71a0a76879428b6101bd
joemand985 <djordjemandic85@gmail.com>
2018-03-02 21:25:56 +0100

    details - change, hide

--- js/NYTE.jsx
+++ js/NYTE.jsx
@@ -22,7 +22,7 @@ $.ajax({
 class Preview extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {title: 'Loading...', description: 'Please wait', img: 'giphy.gif', details: this.props.link, index: this.props.index};
+        this.state = {title: 'Loading...', description: 'Please wait', img: 'giphy.gif', details: this.props.link, index: this.props.index, selected: 0};
     }
 
   componentDidMount(){
@@ -49,10 +49,18 @@ class Preview extends React.Component {
 }
 
   render(){
-console.log(this.state.details)
+    //var det=0
+//console.log(this.state.details)
         const tab = 
             <div>
-                <table class="grid-item" onClick={() => document.getElementById(this.state.index).innerHTML = JSON.stringify(this.state.details.web_url + '<br/>' + this.state.details.snippet + '<br/>' + this.state.details.byline.original + '<br/>' + this.state.details.headline.print_headline + '<br/>' + this.state.details.pub_date + '<br/>' + this.state.details.word_count , null, 2)}>
+                <table class="grid-item" onClick={() => {
+               for (var i=0; i<20; i++){
+               document.getElementById(i).style.display = "none";
+           }
+
+               document.getElementById(this.state.index).style.display = "block"    
+            }
+            }>
     	              <thead>
     	              	<th>News</th>
     	              </thead>
@@ -61,9 +69,14 @@ console.log(this.state.details)
     	              	<tr><td><h3>{this.state.description}</h3></td></tr>
     	              	<tr><td><img src={this.state.img}></img></td></tr>
 
+                        <tr><td id={this.state.index} class="details" >{JSON.stringify(this.state.details.web_url + this.state.details.snippet + '<br/>' + this.state.details.byline.original + '<br/>' + this.state.details.headline.print_headline + '<br/>' + this.state.details.pub_date + '<br/>' + this.state.details.word_count , null, 2)}<input type="submit" value="Hide"  onClick={() => {
+                        document.getElementById(this.state.index).style.color = "red";
+                        document.getElementById(this.state.index).style.visibility = "hidden"
+                        }}/></td></tr>
+
     	              </tbody>
     	               </table>
-    	                <div id={this.state.index} class="details"></div>               
+    	                              
             </div>
   return tab  
 }
